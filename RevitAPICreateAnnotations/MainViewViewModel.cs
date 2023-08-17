2aed2a4e8194f99a0ea0440f2e140fbc65af7c05
andreyzr43 <andreyzr43@yandex.ru>
2023-08-17 07:54:27 +0300

    1

---
+++ RevitAPICreateAnnotations/MainViewViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPITrainingLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICreateAnnotations
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public List<FamilySymbol> Tags { get; }
+        public Pipe Pipe { get; }
+        public DelegateCommand SaveCommand { get; }
+        public FamilySymbol SelectedTagTape { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            Tags=TagsUtils.GetPipeTagTypes(commandData);
+            Pipe = SelectionUtils.GetObject<Pipe>(_commandData, "Выберете трубу");
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var pipeLocCurve = Pipe.Location as LocationCurve;
+            var pipeCurve=pipeLocCurve.Curve;
+            var pipeMidPoint = (pipeCurve.GetEndPoint(0) + pipeCurve.GetEndPoint(1)) / 2;
+
+            using(var ts=new Transaction(doc,"Create tag"))
+            {
+                ts.Start();
+                IndependentTag.Create(doc, SelectedTagTape.Id, doc.ActiveView.Id, new Reference(Pipe), false, TagOrientation.Horizontal, pipeMidPoint);
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+        }
+        public event EventHandler CloseRequest;
+
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);//закрытие окна
+        }
+    }
+}
---
+++ RevitAPICreateDuct/MainViewViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPITrainingLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICreateDuct
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public List<WallType> WallTypes { get; } = new List<WallType>();
+        public List<Level> Levels { get; } = new List<Level>();
+        public DelegateCommand SaveCommand { get; }
+        public double WallHeight { get; set; }
+        public List<XYZ> Points { get; } = new List<XYZ>();
+        public WallType SelectedWallType { get; set; }
+        public Level SelectedLevel { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            WallTypes = RevitAPITrainingLibrary.WallTools.GetWallTypes(commandData);
+            Levels = LevelsUtils.GetLevels(commandData);
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            WallHeight = 100;
+            Points = SelectionUtils.GetPoints(_commandData, "Выберете точки", ObjectSnapTypes.Endpoints);
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (Points.Count < 2 ||
+                SelectedWallType == null ||
+                SelectedLevel == null)
+                return;
+            var curves = new List<Curve>();
+            for (int i = 0; i < Points.Count; i++)
+            {
+                if (i == 0)
+                    continue;
+
+                var prevPoint = Points[i - 1];
+                var currentPoin = Points[i];
+
+                Curve curve = Line.CreateBound(prevPoint, currentPoin);
+                curves.Add(curve);
+            }
+
+            using (var ts = new Transaction(doc, "Create wall"))
+            {
+                ts.Start();
+
+                foreach (var curve in curves)
+                {
+                    Wall.Create(doc, curve, SelectedWallType.Id, SelectedLevel.Id,
+                        UnitUtils.ConvertToInternalUnits(WallHeight, UnitTypeId.Millimeters),
+                        0, false, false);
+                }
+
+                ts.Commit();
+
+            }
+
+            RaiseCloseRequest();
+
+
+        }
+        public event EventHandler CloseRequest;
+
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);//закрытие окна
+        }
+    }
+}
